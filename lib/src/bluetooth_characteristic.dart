558a76e0e57ce703989b4ea4225a03bf87f7312a
Meidi Tönisson <meidi@noomi.com>
2019-12-09 15:10:06 +0100

    Remove commented out code

--- lib/src/bluetooth_characteristic.dart
+++ lib/src/bluetooth_characteristic.dart
@@ -54,8 +54,6 @@ class BluetoothCharacteristic {
           .map((c) {
         // Update the characteristic with the new values
         _updateDescriptors(c.descriptors);
-//        _value.add(c.lastValue);
-//        print('c.lastValue: ${c.lastValue}');
         return c;
       });
 
